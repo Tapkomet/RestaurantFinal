9ebfe1b50d3506914eb48eaf4d69681b6b946400
unknown <romakobzar@ukr.net>
2018-12-25 18:41:42 +0200

    Inbuilt functionality for Item and User added

--- src/main/java/ua/training/model/entity/Item.java
+++ src/main/java/ua/training/model/entity/Item.java
@@ -2,10 +2,90 @@ package ua.training.model.entity;
 
 import lombok.Data;
 
+import java.util.Comparator;
+
 @Data
 public class Item {
-    private int itemId;
+    private int id;
     private String name;
     private long price;
     private boolean available;
+
+    public static Comparator<Item> ItemIdComparator = (s1, s2) -> {
+        int id1 = s1.getId();
+        int id2 = s2.getId();
+        return id1-id2;
+    };
+
+    public static Comparator<Item> ItemNameComparator = (s1, s2) -> {
+        String name1 = s1.getName().toUpperCase();
+        String name2 = s2.getName().toUpperCase();
+        return name1.compareTo(name2);
+    };
+
+    public static Comparator<Item> ItemPriceComparator = (s1, s2) -> {
+        long price1 = s1.getPrice();
+        long price2 = s2.getPrice();
+        return Long.compare(price1, price2);
+    };
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name +
+                ", price=" + price +
+                ", available=" + available +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        final int PRIME = 31;
+        int result = 1;
+        result = result * PRIME + this.id;
+        result = result * PRIME + (available ? 1 : 0);
+        result = result * PRIME + (int) (this.price ^ (this.price >>> 32));
+        return result;
+    }
+
+
+    public static class Builder {
+
+        private int id;
+        private String name;
+        private long price;
+        private boolean available;
+
+        public Builder(int id) {
+            this.id = id;
+        }
+
+        public Builder itemName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder price(long price) {
+            this.price = price;
+            return this;
+        }
+
+        public Builder isAvailable(boolean available) {
+            this.available = available;
+            return this;
+        }
+
+
+
+        public Item build() {
+            Item item = new Item();
+            item.id = this.id;
+            item.name = this.name;
+            item.price = this.price;
+            item.available = this.available;
+            return item;
+        }
+    }
+
 }
--- src/main/java/ua/training/model/entity/User.java
+++ src/main/java/ua/training/model/entity/User.java
@@ -1,11 +1,5 @@
 package ua.training.model.entity;
 
-import lombok.Data;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Data
 public class User {
     private int id;
     private String surname;
@@ -14,12 +8,147 @@ public class User {
     private ROLE role;
 
     public enum ROLE {
-        CLIENT, ADMIN
+        CLIENT, ADMIN, UNKNOWN
     }
 
-    public void setRoleFromString(String role){
+    public User() {
+    }
+
+    public User(int id, String surname, String email, String password) {
+        this.id = id;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(int id, String surname, String email, String password, ROLE role) {
+        this.id = id;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public ROLE getRole() {
+        return role;
+    }
+
+    public void setRole(ROLE role) {
+        this.role = role;
+    }
+
+    public void setRoleFromString(String role) {
         this.role = ROLE.valueOf(role.toUpperCase());
     }
 
-    List<User> users = new ArrayList<>();
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", surname='" + surname +
+                ", email=" + email +
+                ", password=" + password +
+                ", role=" + role +
+                '}';
+    }
+
+    @Override
+    public boolean equals(final java.lang.Object o) {
+        if (o == this) return true;
+        if (o == null) return false;
+        if (o.getClass() != this.getClass()) return false;
+        final User other = (User) o;
+        if (this.id != other.id) return false;
+        if (this.surname == null ? other.surname != null : !this.surname.equals(other.surname)) return false;
+        if (this.email == null ? other.email != null : !this.email.equals(other.email)) return false;
+        if (this.password == null ? other.password != null : !this.password.equals(other.password)) return false;
+        if (this.role == null ? other.role != null : !this.role.equals(other.role)) return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int PRIME = 31;
+        int result = 1;
+        result = result * PRIME + (this.surname == null ? 0 : this.surname.hashCode());
+        result = result * PRIME + (this.email == null ? 0 : this.email.hashCode());
+        result = result * PRIME + (this.password == null ? 0 : this.password.hashCode());
+        result = result * PRIME + (this.role == null ? 0 : this.role.hashCode());
+        return result;
+    }
+
+    public static class Builder {
+
+        private int id;
+        private String surname;
+        private String email;
+        private String password;
+        private ROLE role;
+
+        public Builder(String email) {
+            this.email = email;
+        }
+
+        public Builder id(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder surname(String surname) {
+            this.surname = surname;
+            return this;
+        }
+
+        public Builder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Builder byRole(ROLE role) {
+            this.role = role;
+            return this;
+        }
+
+        public User build() {
+            User user = new User();
+            user.id = this.id;
+            user.surname = this.surname;
+            user.email = this.email;
+            user.password = this.password;
+            user.role = this.role;
+            return user;
+        }
+    }
 }
